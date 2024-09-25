a0c570f32d851b89552141813bed3e2efc071872
Amit Kumar <misamitkumar@yahoo.com>
2024-09-25 09:58:01 +0530

    Update load.php

--- load.php
+++ load.php
@@ -1,17 +1,38 @@
 <?php
 $conn = mysqli_connect("sql5.freesqldatabase.com","sql5733231","FnWURseTd3","sql5733231") or die("Connection Failed.");
-@$block = $_GET['block'];
-@$gp = $_GET['gp'];
-if ($block != "" ) {
-    if ($gp != "") {$sql = "SELECT * FROM demo WHERE Block_Name = '$block' and Nyay_Panchayat = '$gp' "; } 
-    else {$sql = "SELECT * FROM smb_db WHERE Block_Name = '$block'";}; };
 
-$result = mysqli_query($conn, $sql) or die("SQL Failed");
-$output = [];
-if(mysqli_num_rows($result) > 0){
-  while($row = mysqli_fetch_assoc($result)){
-    $output[] = $row;
-  }}else{$output['empty'] = ['empty'];}
-mysqli_close($conn);
-echo json_encode($output);
+$array = "
+[
+	{
+		color: "red",
+		value: "#f00"
+	},
+	{
+		color: "green",
+		value: "#0f0"
+	},
+	{
+		color: "blue",
+		value: "#00f"
+	},
+	{
+		color: "cyan",
+		value: "#0ff"
+	},
+	{
+		color: "magenta",
+		value: "#f0f"
+	},
+	{
+		color: "yellow",
+		value: "#ff0"
+	},
+	{
+		color: "black",
+		value: "#000"
+	}
+]
+";
+
+echo json_encode($array);
 ?>
